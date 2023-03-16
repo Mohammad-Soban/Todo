ca2ba770d46a03a2c5e6078738c40359699ad2f5
Mohammad-Soban <soban1103@gmail.com>
2023-03-16 11:23:33 +0530

    first commit

---
+++ commented.js
@@ -0,0 +1,29 @@
+// var day_msg = ""; 
+    
+//     if (today.getDay() == 0){
+//         day_msg = `<em style="color: #0000e6">Sunday</em> Funday`;
+//     }
+
+//     else if(today.getDay() == 1){
+//         day_msg = `New <em style="color:#d633ff">Monday</em>, New Week, New Goals`;
+//     }
+
+//     else if(today.getDay() == 2){
+//         day_msg = `Its <em style="color:#d633ff">Tuesday,</em> Worry Less, Live More`;
+//     }
+
+//     else if(today.getDay() == 3){
+//         day_msg = `With <em style="color:#d633ff">Wednesday</em>, the calendar goes W.T.F`;
+//     }
+
+//     else if(today.getDay() == 4){
+//         day_msg = `Its <em style="color:#d633ff">Thursday,</em> which is always Thankful`;
+//     }
+
+//     else if(today.getDay() == 5){
+//         day_msg = `Finally <em style="color: #d633ff">Friday</em>, Friyay`;
+//     }
+
+//     else if(today.getDay() == 6){
+//         day_msg = `My Mind goes: <em style="color:#0000e6">'Saturday Saturday'</em>`;
+//     }    
